c9aae4bd10a437dadc0f7d6ae5f35b452824c472
Dorian Penin <dorian.penin@gmail.com>
2025-07-02 17:04:44 +0200

    feat: improved spacing

--- src/app/_components/accueil/HomePageComponent.tsx
+++ src/app/_components/accueil/HomePageComponent.tsx
@@ -4,21 +4,21 @@ import PilotageSection from "./pilotage/PilotageSection";
 
 const HomePageComponent: FC = () => {
   return (
-    <div className="bg-grey h-full p-6">
-      <div className="flex h-full gap-6">
-        <div className="flex min-w-96 flex-col gap-6">
-          <div className="bg-dark-grey border-grey rounded-lg border p-6 shadow-lg">
+    <div className="bg-grey h-full p-4">
+      <div className="flex h-full gap-4">
+        <div className="flex min-w-96 flex-col gap-4">
+          <div className="bg-dark-grey border-grey rounded-lg border p-4 shadow-lg">
             <PilotageSection />
           </div>
 
-          <div className="bg-dark-grey border-grey rounded-lg border p-6 shadow-lg">
+          <div className="bg-dark-grey border-grey rounded-lg border p-4 shadow-lg">
             <h3 className="mb-4 text-lg font-semibold text-white">Capteurs</h3>
           </div>
 
-          <div className="border-grey flex-grow rounded-lg border bg-white p-6 shadow-lg"></div>
+          <div className="border-grey flex-grow rounded-lg border bg-white p-4 shadow-lg"></div>
         </div>
 
-        <div className="flex w-80 min-w-0 flex-grow basis-0 flex-col gap-6">
+        <div className="flex w-80 min-w-0 flex-grow basis-0 flex-col gap-4">
           <div className="bg-dark-grey border-grey relative flex-grow overflow-hidden rounded-lg border shadow-lg">
             <Image
               src="/cuve_tmp.png"
@@ -28,7 +28,7 @@ const HomePageComponent: FC = () => {
             />
           </div>
 
-          <div className="bg-dark-grey border-grey rounded-lg border p-6 shadow-lg">
+          <div className="bg-dark-grey border-grey rounded-lg border p-4 shadow-lg">
             <h3 className="mb-4 text-lg font-semibold text-white">
               Information
             </h3>
