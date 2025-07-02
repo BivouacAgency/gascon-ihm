d1df503f0f31c3be1daeaf6b86fa3f97ddfcae9f
Dorian Penin <dorian.penin@gmail.com>
2025-07-02 18:54:56 +0200

    feat: open sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Navigation } from "./_components/Navigation";
 
@@ -11,16 +11,16 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const openSans = Open_Sans({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-open-sans",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${openSans.variable}`}>
       <body className="bg-grey min-h-screen">
         <TRPCReactProvider>
           <div className="flex h-screen">
