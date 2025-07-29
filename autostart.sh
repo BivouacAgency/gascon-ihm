97ee3f06c8710e22d1444bd2ed222944575161c7
Dorian PENIN <dorian.penin@gmail.com>
2025-07-29 14:57:13 +0200

    fix: autostart chromium without kiosk or fullscreen to allow keyboard

--- autostart.sh
+++ autostart.sh
@@ -6,4 +6,4 @@ export PATH="$HOME/.local/share/pnpm:$HOME/.nvm/versions/node/v22.14.0/bin:$PATH
 cd /home/test/gascon-ihm || exit 1
 pnpm dev:full >> /home/test/pnpm.log 2>&1 &
 sleep 10
-chromium-browser --kiosk http://localhost:3000 >> /home/test/chromium.log 2>&1 &
+chromium-browser --app=http://localhost:3000 --window-size=1280,720 >> /home/test/chromium.log 2>&1 &
